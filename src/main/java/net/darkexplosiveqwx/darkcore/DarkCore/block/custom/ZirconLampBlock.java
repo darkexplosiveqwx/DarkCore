da1c0af82d2f3e29d0f6d1c5938ce1a4caff8f1e
darkexplosiveqwx <the.qwx.channel@gmail.com>
2022-08-02 16:28:30 +0200

    changed ZirconLampBlock added some @NotNull

--- src/main/java/net/darkexplosiveqwx/darkcore/DarkCore/block/custom/ZirconLampBlock.java
+++ src/main/java/net/darkexplosiveqwx/darkcore/DarkCore/block/custom/ZirconLampBlock.java
@@ -1,11 +1,8 @@
 package net.darkexplosiveqwx.darkcore.DarkCore.block.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -14,6 +11,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 
 public class ZirconLampBlock extends Block {
     public static final BooleanProperty LIT = BooleanProperty.create("lit");
@@ -23,7 +21,7 @@ public class ZirconLampBlock extends Block {
         super(p_49795_);
     }
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result) {
+    public @NotNull InteractionResult use(@NotNull BlockState state, Level level, @NotNull BlockPos blockPos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult result) {
         if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND && player.getMainHandItem() == ItemStack.EMPTY) {
            level.setBlock(blockPos, state.cycle(LIT),3);
         }
@@ -32,8 +30,8 @@ public class ZirconLampBlock extends Block {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> bulider) {
-        bulider.add(LIT);
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
     }
 
 }

b3825a31869f44ee1a6a5ed68d6c2107f27ff07e
darkexplosiveqwx <the.qwx.channel@gmail.com>
2022-09-14 17:39:44 +0200

    fixed module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,12 @@
 module darkcore.main {
+    requires forge;
+    requires org.jetbrains.annotations;
+    requires org.slf4j;
+    requires logging;
+    requires javafmllanguage;
+    requires net.minecraftforge.eventbus;
+    requires jei;
+    requires geckolib.forge;
+    requires mekanism;
+    requires jline.terminal;
 }
